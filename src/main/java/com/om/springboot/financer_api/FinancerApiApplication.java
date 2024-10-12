cbe5340a38271b5bf9f557c5d82a191aaf107b32
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2024-10-12 17:50:15 +0545

    First commit

---
+++ src/main/java/com/om/springboot/financer_api/FinancerApiApplication.java
@@ -0,0 +1,13 @@
+package com.om.springboot.financer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancerApiApplication.class, args);
+	}
+
+}
